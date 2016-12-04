b9d61fb7ae157394ce9284da16e788634ee32367
Alna132 <alannacurran@gmail.com>
2016-12-04 15:38:31 +0000

    Buttons and code for hidden objects added

--- MobileApps3-Project/MobileApps3-Project/graveLevel.xaml.cs
+++ MobileApps3-Project/MobileApps3-Project/graveLevel.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +14,102 @@ namespace MobileApps3_Project
         public graveLevel()
         {
             this.InitializeComponent();
-        }
-    }
-}
+            btnHat.Visibility = Visibility.Visible;
+
+        }//- End of graveLevel
+
+        //private void Image_Tapped(object sender, TappedRoutedEventArgs e)
+        //{
+        //    Point tappedPoint = e.GetPosition(graveScene);
+
+        //    //if (tappedPoint.X == )
+
+        //}
+
+        private void btnHat_Click(object sender, RoutedEventArgs e)
+        {
+            btnHat.Visibility = Visibility.Collapsed;
+            txtHat.Visibility = Visibility.Collapsed;
+
+            if(txtHat.Visibility == Visibility.Collapsed && txtTeddy.Visibility == Visibility.Collapsed && 
+                txtEggplant.Visibility == Visibility.Collapsed && txtCherry.Visibility == Visibility.Collapsed && 
+                txtLamp.Visibility == Visibility.Collapsed)
+            {
+                var messageDialog = new MessageDialog("Congratulations! You found all the items in this level!");
+                messageDialog.Commands.Add(new UICommand("Back", new UICommandInvokedHandler(this.CommandInvokedHandler)));
+
+            }//- End of if
+        }//- End of btnHat_Clicked
+
+        private void btnTeddy_Click(object sender, RoutedEventArgs e)
+        {
+            btnTeddy.Visibility = Visibility.Collapsed;
+            txtTeddy.Visibility = Visibility.Collapsed;
+
+            if (txtHat.Visibility == Visibility.Collapsed && txtTeddy.Visibility == Visibility.Collapsed &&
+                    txtEggplant.Visibility == Visibility.Collapsed && txtCherry.Visibility == Visibility.Collapsed &&
+                    txtLamp.Visibility == Visibility.Collapsed)
+            {
+                var messageDialog = new MessageDialog("Congratulations! You found all the items in this level!");
+                messageDialog.Commands.Add(new UICommand("Back", new UICommandInvokedHandler(this.CommandInvokedHandler)));
+
+            }//- End of if
+
+        }//- End of btnTeddy_Clicked
+
+        private void btnEggplant_Click(object sender, RoutedEventArgs e)
+        {
+            btnEggplant.Visibility = Visibility.Collapsed;
+            txtEggplant.Visibility = Visibility.Collapsed;
+
+            if (txtHat.Visibility == Visibility.Collapsed && txtTeddy.Visibility == Visibility.Collapsed &&
+                    txtEggplant.Visibility == Visibility.Collapsed && txtCherry.Visibility == Visibility.Collapsed &&
+                    txtLamp.Visibility == Visibility.Collapsed)
+            {
+                var messageDialog = new MessageDialog("Congratulations! You found all the items in this level!");
+                messageDialog.Commands.Add(new UICommand("Back", new UICommandInvokedHandler(this.CommandInvokedHandler)));
+
+            }//- End of if
+
+        }//- End of btnEggplant_Click
+
+        private void btnCherry_Click(object sender, RoutedEventArgs e)
+        {
+            btnCherry.Visibility = Visibility.Collapsed;
+            txtCherry.Visibility = Visibility.Collapsed;
+
+            if (txtHat.Visibility == Visibility.Collapsed && txtTeddy.Visibility == Visibility.Collapsed &&
+                    txtEggplant.Visibility == Visibility.Collapsed && txtCherry.Visibility == Visibility.Collapsed &&
+                    txtLamp.Visibility == Visibility.Collapsed)
+            {
+                var messageDialog = new MessageDialog("Congratulations! You found all the items in this level!");
+                messageDialog.Commands.Add(new UICommand("Back", new UICommandInvokedHandler(this.CommandInvokedHandler)));
+
+            }//- End of if
+
+        }//- End of btnCherry_Click
+
+        private void btnLamp_Click(object sender, RoutedEventArgs e)
+        {
+            btnLamp.Visibility = Visibility.Collapsed;
+            txtLamp.Visibility = Visibility.Collapsed;
+
+            if (txtHat.Visibility == Visibility.Collapsed && txtTeddy.Visibility == Visibility.Collapsed &&
+                    txtEggplant.Visibility == Visibility.Collapsed && txtCherry.Visibility == Visibility.Collapsed &&
+                    txtLamp.Visibility == Visibility.Collapsed)
+            {
+                var messageDialog = new MessageDialog("Congratulations! You found all the items in this level!");
+                messageDialog.Commands.Add(new UICommand("Back", new UICommandInvokedHandler(this.CommandInvokedHandler)));
+
+            }//- End of if
+
+        }//- End of btnLamp_Click
+
+        private void CommandInvokedHandler(IUICommand command)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }//- End of CommandInvokedHandler
+
+    }//- End of graveLevel
+
+}//- End of MobileApps3_Project
